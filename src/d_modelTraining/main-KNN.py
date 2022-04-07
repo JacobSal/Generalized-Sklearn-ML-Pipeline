2ad1ffa042d39a9c870d51dceee540cfb5267338
eduluca <78570829+eduluca@users.noreply.github.com>
2022-04-07 16:40:46 -0400

    Four models working and ready to run with gridsearch

--- src/d_modelTraining/main-KNN.py
+++ src/d_modelTraining/main-KNN.py
@@ -20,11 +20,12 @@ import cv2
 import dill as pickle
 
 
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, GridSearchCV
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import RobustScaler
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score,f1_score,roc_auc_score,roc_curve,auc
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn_evaluation import plot
 
 from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
 
@@ -56,27 +57,39 @@ def robust_save(fname):
     plt.savefig(join(saveBin,'overlayed_predictions.png',dpi=200,bbox_inches='tight'))
 #enddef
 
-def objective(space):
-    clf=KNeighborsClassifier(
-                    # n_estimators =space['n_estimators'], max_depth = int(space['max_depth']), gamma = space['gamma'],
-                    # reg_alpha = int(space['reg_alpha']),min_child_weight=int(space['min_child_weight']),
-                    # colsample_bytree=int(space['colsample_bytree']))
+# def objective(space):
+#     clf=KNeighborsClassifier(
+#                     # n_estimators =space['n_estimators'], max_depth = int(space['max_depth']), gamma = space['gamma'],
+#                     # reg_alpha = int(space['reg_alpha']),min_child_weight=int(space['min_child_weight']),
+#                     # colsample_bytree=int(space['colsample_bytree']))
     
-    evaluation = [( X_train, y_train), ( X_test, y_test)]
+#     evaluation = [( X_train, y_train), ( X_test, y_test)]
     
-    clf.fit(X_train, y_train,
-            eval_set=evaluation, eval_metric="auc",
-            early_stopping_rounds=10,verbose=False)
+#     clf.fit(X_train, y_train,
+#             eval_set=evaluation, eval_metric="auc",
+#             early_stopping_rounds=10,verbose=False)
     
 
-    pred = clf.predict(X_test)
-    accuracy = accuracy_score(y_test, pred>0.5)
-    print ("SCORE:", accuracy)
-    return {'loss': -accuracy, 'status': STATUS_OK }
-#enddef
+#     pred = clf.predict(X_test)
+#     accuracy = accuracy_score(y_test, pred>0.5)
+#     print ("SCORE:", accuracy)
+#     return {'loss': -accuracy, 'status': STATUS_OK }
+# #enddef
+
+def plot_roc_curve(roc_auc_train, roc_auc_test):
+    plt.figure(0)
+    plt.title('Receiver Operating Characteristic')
+    plt.plot(fpr_tr, tpr_tr, 'g', label = 'Training AUC = %0.2f' % roc_auc_train)
+    plt.plot(fpr_ts, tpr_ts, 'b', label = 'Testing AUC = %0.2f' % roc_auc_test)
+    plt.legend(loc = 'lower right')
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.show()
 
 #%% PARAMS
-param_grid = {'kneighborsclassifier__n_neighbors':[5,7,10,13,15,18,20]}
 dTime = '19032022' #date.today().strftime('%d%m%Y')
 
 #%%
@@ -94,55 +107,71 @@ print("y_train: " + str(np.unique(y_train)))
 print("y_test: " + str(np.unique(y_test)))
 
 #%% Create KNN pipeline
-pipe_knn = make_pipeline(RobustScaler(),KNeighborsClassifier())
+print('KNN:')
+# pipe_knn = make_pipeline(RobustScaler(),KNeighborsClassifier())
 
-#%% SVM MODEL FITTING
-#we create an instance of SVM and fit out data.
+#%% KNN MODEL FITTING
+#we create an instance of KNN and fit out data.
 print("starting modeling career...")
 
+#%% GRIDSEARCH
+print("Gridsearch with cross-validation initializing...")
 
-#%% GRID SEARCH
-# print("Gridsearch with cross-validation initializing...")
+#Parameter Grid with ranges
+paraGrid = {'n_neighbors': np.arange(1,5,2),
+            'weights': ['uniform', 'distance']}
+            # 'metric': ['euclidean', 'manhattan']}
 
-# #Parameter Grid with ranges
-# param_grid = {'max_depth': [0,9999],
-#               'min_samples_split': ,
-#               'max_leaf_nodes': , 
-#               'min_samples_leaf': ,
-#               'n_estimators': ,
-#                'max_samples': ,
-#                'max_features': }
-
-# #pipe_knn.set_params(kneighborsclassifier__n_neighbors = 7)
-
-#%% GRIDSEARCH (IF NECESSARY)
-# gs = GridSearchCV(estimator = pipe_knn,
-#                   param_grid = param_grid,
-#                   scoring = 'roc_auc',
-#                   cv = 5,
-#                   n_jobs = -1,
-#                   verbose = 10)
+knn_gridsearch = GridSearchCV(estimator = KNeighborsClassifier(),
+                  param_grid = paraGrid,
+                  scoring = 'roc_auc',
+                  cv = 2,
+                  n_jobs = -1,
+                  verbose = 10)
 
 
-# print("Fitting...")
-# gs = gs.fit(X_train,y_train)
-# print('best score: ' + str(gs.best_score_))
-# print(gs.best_params_)
-# pipe_knn = gs.best_estimator_
-### END Gridsearch ####
+knn_gridsearch.fit(X_train,y_train)
+best_score = knn_gridsearch.best_score_
+best_params = knn_gridsearch.best_params_
+print('Best Params: ', best_params)
+print('Best Score: ', best_score)
+print('CV Results: ', knn_gridsearch.cv_results_)
+
+#Plotting parameter performance
+print('Plotting Gridsearch Results...')
+grid_scores = knn_gridsearch.cv_results_
+
 
 #%% MODEL FITTING
-model = pipe_knn.fit(X_train,y_train)
+print('fitting...')
+clf = KNeighborsClassifier(**best_params)
+clf.fit(X_train,y_train)
 #y_score = model.decision_function(X_test)
-print(model.score(X_test,y_test))
+print(clf.score(X_test,y_test))
 filename = join(modelDir,('fittedKNN_'+dTime+'.sav'))
-pickle.dump(model, open(filename, 'wb'))
+pickle.dump(clf, open(filename, 'wb'))
 print('done')
 
-y_predict = model.predict(X_test)
-y_train_predict = model.predict(X_train)
+#Result metrics 
+y_train_predict = clf.predict(X_train)
+y_predict = clf.predict(X_test)
 print('KNN Train accuracy',accuracy_score(y_train, y_train_predict))
 print('KNN Test accuracy',accuracy_score(y_test,y_predict))
+print('KNN Train F1 Score', f1_score(y_train, y_train_predict))
+print('KNN Test F1 score', f1_score(y_test, y_predict))
+print('KNN Train ROC_AUC Score', roc_auc_score(y_train, clf.predict_proba(X_train)[:,1]))
+print('KNN Test ROC_AUC score', roc_auc_score(y_test, clf.predict_proba(X_test)[:,1]))
+
+#train data ROC
+fpr_tr, tpr_tr, threshold = roc_curve(y_train, clf.predict_proba(X_train)[:,1])
+roc_auc_train = auc(fpr_tr, tpr_tr)
+
+#test data ROC
+fpr_ts, tpr_ts, threshold = roc_curve(y_test, clf.predict_proba(X_test)[:,1])
+roc_auc_test = auc(fpr_ts, tpr_ts)
+
+#Plot ROC curve
+plot_roc_curve(roc_auc_train, roc_auc_test)
 #%% CROSS VALIDATE
 # scores = cross_val_score(estimator = pipe_knn,
 #                           X = X,
--- src/d_modelTraining/main-rndmFrst.py
+++ src/d_modelTraining/main-rndmFrst.py
@@ -20,8 +20,9 @@ import dill as pickle
 
 from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score, GridSearchCV
-from sklearn.metrics import   accuracy_score,f1_score
-from sklearn.ensemble import  RandomForestClassifier
+from sklearn.metrics import accuracy_score,f1_score,roc_auc_score,roc_curve,auc
+from sklearn.ensemble import RandomForestClassifier
+from sklearn_evaluation import plot
 
 from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
 
@@ -52,21 +53,32 @@ def robust_save(fname):
     plt.savefig(join(saveBin,'overlayed_predictions.png',dpi=200,bbox_inches='tight'))
 #enddef
 
-def objective(space):
-    clf=RandomForestClassifier(
-                    n_estimators =space['n_estimators'], max_depth = space['max_depth'], min_samples_split = int(space['min_samples_split']))
-                    #max_leaf_nodes = int(space['max_leaf_nodes']), min_samples_leaf= int(space['min_samples_leaf']),
-                    #max_features = int(space['max_features']))
+# def objective(space):
+#     clf=RandomForestClassifier(
+#                     n_estimators =space['n_estimators'], max_depth = space['max_depth'], min_samples_split = int(space['min_samples_split']))
+#                     #max_leaf_nodes = int(space['max_leaf_nodes']), min_samples_leaf= int(space['min_samples_leaf']),
+#                     #max_features = int(space['max_features']))
     
     
-    clf.fit(X_train, y_train)
-    
+#     clf.fit(X_train, y_train)
+#     pred = clf.predict(X_test)
+#     accuracy = accuracy_score(y_test, pred>0.5)
+#     print ("SCORE:", accuracy)
+#     return {'loss': -accuracy, 'status': STATUS_OK }
+# #enddef
 
-    pred = clf.predict(X_test)
-    accuracy = accuracy_score(y_test, pred>0.5)
-    print ("SCORE:", accuracy)
-    return {'loss': -accuracy, 'status': STATUS_OK }
-#enddef
+def plot_roc_curve(roc_auc_train, roc_auc_test):
+    plt.figure(0)
+    plt.title('Receiver Operating Characteristic')
+    plt.plot(fpr_tr, tpr_tr, 'g', label = 'Training AUC = %0.2f' % roc_auc_train)
+    plt.plot(fpr_ts, tpr_ts, 'b', label = 'Testing AUC = %0.2f' % roc_auc_test)
+    plt.legend(loc = 'lower right')
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.show()
 
 # def plot_grid_search(cv_results, grid_param_1, grid_param_2, name_param_1, name_param_2):
 #     # Get Test Scores Mean and std for each grid search
@@ -123,35 +135,47 @@ print('Random Forest:')
 print("Gridsearch with cross-validation initializing...")
 
 #Parameter Grid with ranges
-paraGrid = {'max_depth': np.arange(10,210,20),
-              'min_samples_split': np.arange(100,400,20),
-              'max_leaf_nodes': np.arange(60,210,50),
-              'min_samples_leaf': np.arange(10,50,10),
-              'n_estimators': np.arange(100,200,50),
-              'max_samples': np.arange(0.1,0.9,0.4)}
-                #'max_features': np.arange(5,10,5)}
+paraGrid = {'criterion': ['gini','entropy'],
+            'min_samples_leaf': np.arange(1,3,1)}
+            # 'min_samples_split': np.arange(2,20,2),
+            # 'n_estimators': np.arange(100,500,200),
+            # 'max_features': ['auto','sqrt','log2']}
+
+             # 'max_depth': np.arange(10,210,20),
+              #'max_leaf_nodes': np.arange(60,210,50)
+              #'max_samples': np.arange(0.1,0.9,0.4)
+              
 
 
 #Gridsearch with cross-validation
 rf_gridsearch = GridSearchCV(estimator = RandomForestClassifier(), 
                         param_grid = paraGrid, 
-                        scoring = 'f1', # can use make_scorer(custom_scoring_function, greater_is_better=True)
-                        cv = 2, 
-                        verbose = 2, 
+                        scoring = 'roc_auc', # can use make_scorer(custom_scoring_function, greater_is_better=True)
+                        cv = 3, 
+                        verbose = 10, 
                         n_jobs = -1)
 
 #Calling Method 
-# plot_grid_search(pipe_grid.cv_results_, n_estimators, max_features, 'N Estimators', 'Max Features')
+# plot_grid_search(rf_gridsearch.cv_results_, n_estimators, max_features, 'N Estimators', 'Max Features')
 
 rf_gridsearch.fit(X_train,y_train)
-bestparams = rf_gridsearch.best_params_
-print(bestparams)
+best_score = rf_gridsearch.best_score_
+best_params = rf_gridsearch.best_params_
+print('Best Params: ', best_params)
+print('Best Score: ', best_score)
+print('CV Results: ', rf_gridsearch.cv_results_)
 
 #Plotting parameter performance
+print('Plotting Gridsearch Results...')
+grid_scores = rf_gridsearch.cv_results_
 
-# grid_scores = ref_gridsearch.cv_results_
-# plot.grid_search(rf_gridsearch.cv_results_, change=('max_depth', 'min_samples_split'),
-#                  subset={'max_leaf_nodes': ''})
+# plt.figure(0)
+# plot.grid_search(grid_scores, change=('min_samples_leaf', 'min_samples_split'),
+#                  subset={'n_estimators': [100]})
+
+# plt.figure(1)
+# plot.grid_search(grid_scores, change='n_estimators', kind='bar')
+
 
 #%% BAYESIAN OPTIMIZATION WITH HYPEROPT
 
@@ -182,26 +206,36 @@ print(bestparams)
 
 
 #%% MODEL FITTING
-rf_gridsearch.fit(X_train,y_train)
-bestparams = rf_gridsearch.best_params_
-print(bestparams)
-
 print('fitting...')
-RFmodel = RandomForestClassifier(**bestparams)
-model = RFmodel.fit(X_train,y_train)
+clf = RandomForestClassifier(**best_params)
+clf.fit(X_train,y_train)
 #y_score = model.decision_function(X_test)
-print(model.score(X_test,y_test))
+print(clf.score(X_test,y_test))
 filename = join(modelDir,('fittedRF_'+dTime+'.sav'))
-pickle.dump(model, open(filename, 'wb'))
+pickle.dump(clf, open(filename, 'wb'))
 print('done')
 
-
-y_train_predict = model.predict(X_train)
-y_predict = model.predict(X_test)
+#Result metrics 
+y_train_predict = clf.predict(X_train)
+y_predict = clf.predict(X_test)
 print('RF Train accuracy',accuracy_score(y_train, y_train_predict))
 print('RF Test accuracy',accuracy_score(y_test,y_predict))
 print('RF Train F1 Score', f1_score(y_train, y_train_predict))
 print('RF Test F1 score', f1_score(y_test, y_predict))
+print('RF Train ROC_AUC Score', roc_auc_score(y_train, clf.predict_proba(X_train)[:,1]))
+print('RF Test ROC_AUC score', roc_auc_score(y_test, clf.predict_proba(X_test)[:,1]))
+
+#train data ROC
+fpr_tr, tpr_tr, threshold = roc_curve(y_train, clf.predict_proba(X_train)[:,1])
+roc_auc_train = auc(fpr_tr, tpr_tr)
+
+#test data ROC
+fpr_ts, tpr_ts, threshold = roc_curve(y_test, clf.predict_proba(X_test)[:,1])
+roc_auc_test = auc(fpr_ts, tpr_ts)
+
+#Plot ROC curve
+plot_roc_curve(roc_auc_train, roc_auc_test)
+
 
 
 #%% Cross Validate
